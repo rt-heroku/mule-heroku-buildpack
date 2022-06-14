2e7fb13599f36832beff8dd8f255cbf5357e6466
rt-heroku <rodrigo.torres@mulesoft.com>
2022-06-14 10:11:04 -0400

    Initial

--- lib/common.sh
+++ lib/common.sh
@@ -59,6 +59,18 @@ detect_mule_version() {
   fi
 }
 
+get_app_system_value() {
+  local file=${1?"No file specified"}
+  local key=${2?"No key specified"}
+
+  # escape for regex
+  local escaped_key=$(echo $key | sed "s/\./\\\./g")
+
+  [ -f $file ] && \
+  grep -E ^$escaped_key[[:space:]=]+ $file | \
+  sed -E -e "s/$escaped_key([\ \t]*=[\ \t]*|[\ \t]+)([A-Za-z0-9\.-]*).*/\2/g"
+}
+
 cache_copy() {
   rel_dir=$1
   from_dir=$2
